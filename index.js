94c5ab02e6fdb6fcd7cb6aeeea991bd76dc252d4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-02-07 09:52:19 -0500

    testing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var check = require('check-types');
 var chalk = require('chalk');
 var S = require('string');
+var exists = require('fs').existsSync;
 var read = require('fs').readFileSync;
 var pkg = require('./package.json');
 
@@ -9,13 +10,16 @@ if (!check.unemptyString(filename)) {
   console.error('usage', pkg.name, '<input filename>');
   process.exit(-1);
 }
-
-var todoRegexp = /\ todo/gi;
-var todoFormatRegexp = /\ TODO\(\w+\):/g;
+if (!exists(filename)) {
+  console.error(chalk.red('could not find file', filename));
+  process.exit(-1);
+}
 
 var content = read(filename, 'utf-8');
 var lines = S(content).lines();
 lines.forEach(function (line, k) {
+  var todoRegexp = /\ todo/gi;
+  var todoFormatRegexp = /\s*TODO\(\w+\):/g;
   if (todoRegexp.test(line)) {
     var valid = todoFormatRegexp.test(line);
     var msg = k +': ' + line;
